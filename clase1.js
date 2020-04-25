d64da5f93fab40d161c6a986082fa0cea0351229
lbardalezg <lbardalezg@gmail.com>
2020-04-25 18:34:55 -0500

    Clase 1 - Variables

---
+++ clase1.js
@@ -0,0 +1,7 @@
+var nombre = 'Sacha', apellido = 'Simpson'
+var edad = 28
+console.log('Hola ' + nombre + ' ' + apellido)
+edad = '30 años'
+console.log('Tengo ' + edad + ' años')
+peso = '75kg';
+console.log(peso)
