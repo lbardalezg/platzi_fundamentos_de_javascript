30ba9dbe0054403ca02a08b915a69f11d63be98a
lbardalezg <lbardalezg@gmail.com>
2020-04-26 02:19:52 -0500

    Clase 15 - Estructuras repetitivas: do-while

---
+++ clase15.js
@@ -0,0 +1,10 @@
+var contador = 0;
+
+const llueve = () => Math.random() < 0.25;
+do {
+    contador++;
+} while (!llueve())
+
+var cantidad = veces => veces === 1 ? "vez" : "veces";
+
+console.log(`Fui a ver si llovia ${contador} ${cantidad(contador)}`)
